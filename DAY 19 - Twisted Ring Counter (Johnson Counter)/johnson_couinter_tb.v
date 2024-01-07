2ff91a675bf9d8357122ef6c72f7c951d0f72d6b
Indra-Saha <149527592+Indra-Saha@users.noreply.github.com>
2024-01-08 00:32:13 +0530

    Add files via upload

---
+++ DAY 19 - Twisted Ring Counter (Johnson Counter)/johnson_couinter_tb.v
@@ -0,0 +1,48 @@
+module johnson_counter_tb;
+
+	reg clk,rst,in,load;
+	wire[3:0]q;
+
+	johnson_counter DUT(clk,rst,load,in,q);
+
+	initial
+		begin
+			clk=1'b1;
+			forever #10 clk=~clk;
+		end
+
+	task reset;
+	begin
+		@(negedge clk);
+			rst=1'b1;
+		@(negedge clk);
+			rst=1'b0;
+	end
+	endtask
+
+	task inputs;
+	input i;
+	begin
+		@(negedge clk);
+			load=1'b1;
+			in=i;
+		@(negedge clk);
+			load=1'b0;
+	end
+	endtask
+
+	initial
+		begin
+			reset;
+			repeat(7)
+			@(negedge clk);
+			inputs(1'b1);
+			repeat(2)
+			@(negedge clk);
+			inputs(1'b1);
+			repeat(5)
+			@(negedge clk);
+			$finish;
+		end
+	
+endmodule
---
+++ DAY 19 - Twisted Ring Counter (Johnson Counter)/johnson_counter.v
@@ -0,0 +1,20 @@
+module johnson_counter(clk,rst,load,in,q);
+
+	input clk,rst,in,load;
+	output reg[3:0]q;
+
+	always@(posedge clk)
+	begin
+		if(rst)
+			q<=4'b0001;
+		else
+			begin
+				if(load)
+					q<={q[2:0],in};
+				else
+					q<={q[2:0],~q[3]};
+			end
+	end
+
+endmodule
+				
